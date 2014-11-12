a8da409349a6f9b52856aec561362fef5f065e46
Kenneth Wilcox <wilcoxk@gmail.com>
2014-11-12 15:42:07 -0700

    Demo changes

--- CC.Common.JSON.Demo/frmMain.cs
+++ CC.Common.JSON.Demo/frmMain.cs
@@ -28,8 +28,7 @@ namespace CC.Common.JSON.Demo
 
     private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
     {
-      SetPreferences();
-      prefs.Save(@"C:\Temp\Test.json");
+      SavePreferences();
     }
 
     private void btnDefaults_Click(object sender, EventArgs e)
@@ -43,20 +42,27 @@ namespace CC.Common.JSON.Demo
       prefs.Load(@"C:\Temp\Test.json");
       checkBox1.Checked = prefs.Get("checkBox", false);
       DateTime now = DateTime.Now;
-      dateTimePicker1.Value = prefs.Get("dateTime", new DateTime(now.Year, now.Month, now.Day));
+      dateTimePicker1.Value = prefs.Get("dateTime", new DateTime(now.Year, now.Month, now.Day, 23, 0, 0));
       textBox1.Text = prefs.Get("text", String.Empty);
       numericUpDown1.Value = prefs.Get("integer", 0);
       _openCount = prefs.Get("float", 0.0f);
       Text = _openCount.ToString();
+      this.Size = prefs.Get("size", new Size(314, 202));
+      this.Location = prefs.Get("location", new Point(0, 0));
     }
     
-    private void SetPreferences()
+    private void SavePreferences()
     {
       prefs.Set("checkBox", checkBox1.Checked);
       prefs.Set("dateTime", dateTimePicker1.Value);
       prefs.Set("text", textBox1.Text);
       prefs.Set("integer", numericUpDown1.Value);
       prefs.Set("float", ++_openCount);
+
+      prefs.Set("size", this.Size);
+      prefs.Set("location", this.Location);
+
+      prefs.Save(@"C:\Temp\Test.json");
     }
   }
 }
